741b5e1f405a451336aad524c7e48e93b746d5a9
Karthik <karthik.dhanalekshmi@gmail.com>
2026-03-25 10:32:29 +0530

    changehfdfi

--- frontend/src/config/api/axiosInstance.ts
+++ frontend/src/config/api/axiosInstance.ts
@@ -1,8 +1,7 @@
-// src/api/axiosInstance.js
+// frontend/src/config/api/axiosInstance.ts
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-console.log('API Base URL:', BASE_URL); // For debugging
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
