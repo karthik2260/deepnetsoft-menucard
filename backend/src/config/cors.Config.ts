b1cb96f3f8cf194457181ae8f65425c1182b867c
Karthik <karthik.dhanalekshmi@gmail.com>
2026-03-25 11:25:46 +0530

    fdkfm

--- backend/src/config/cors.Config.ts
+++ backend/src/config/cors.Config.ts
@@ -1,10 +1,10 @@
 // backend/src/config/cors.Config.ts
 export const corsOption = {
   origin: [
-    'http://localhost:5173', // for local development
+    'http://localhost:5173', // local frontend
     'https://deepnetsoft-menucard-app.vercel.app' // deployed frontend
   ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // if using cookies
+  methods: ['GET','POST','PUT','DELETE','OPTIONS','PATCH'],
+  allowedHeaders: ['Content-Type','Authorization'],
+  credentials: true,
 };
